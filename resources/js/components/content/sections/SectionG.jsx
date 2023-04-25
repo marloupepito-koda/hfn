9d8e0a777701010041015f59bfcece74f45487c1
marlou-pepito-koda <marlou.pepito@kodakollectiv.com>
2023-04-25 10:39:01 +0800

    added section d

--- resources/js/components/content/sections/SectionG.jsx
+++ resources/js/components/content/sections/SectionG.jsx
@@ -14,14 +14,14 @@ function SectionA() {
         <>
             <Row1 />
             <Row2 />
-            <Row3 />
-            <Row4 />
+            {/* <Row3 />
+            <Row4 /> */}
             <Row5 />
             <Row6 />
             <Row7 />
             <Row8 />
             <Row9 />
-            <Row10 />
+            {/* <Row10 /> */}
         </>
     );
 }
